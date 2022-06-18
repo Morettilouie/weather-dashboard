9b017e7689a8178fd7072d99f329001fbf73b271
Louie Moretti <morettilouie@gmail.com>
2022-06-18 01:11:30 -0500

    future weather stored, need replaced when new search

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,8 +22,12 @@ var getCityInfo = function(city) {
         });
     });
 };
+
+var futureContainerEl = document.querySelector(".future-container");
+var futureBoxEl = document.querySelector(".future-box");
 var getWeatherInfo = function(city, lat, lon) {
     apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&exclude={part}&appid=cf5d67cbcf13a4639df8ab5a787fe6cf";
+    
     
     fetch(apiUrl).then(function (response) {
         response.json().then(function(data) {
@@ -33,19 +37,39 @@ var getWeatherInfo = function(city, lat, lon) {
 
             var temp = data.current.temp;
             temp = (temp - 273.15) * 9/5 + 32;
+            temp = temp.toFixed(2);
             var humid = data.current.humidity; 
             windSpeed = data.current.wind_speed;
             UV = data.current.uvi;            
             forecastDisplay(temp, humid, windSpeed, UV);
-
+            
             for (var i = 0; i < 5; i++) {
+                
                 var futureTemp = data.daily[i].temp.day;
-                console.log(futureTemp);
+                futureTemp = (futureTemp - 273.15) * 9/5 + 32;
+                futureTemp = futureTemp.toFixed(2);
                 var futureWind = data.daily[i].wind_speed;
-                console.log(futureWind);
                 var futureHumidity = data.daily[i].humidity;
-                console.log(futureHumidity);
-                futureWeatherConditions(futureTemp, futureWind, futureHumidity)
+
+                // create a future weather box div
+                var futureDisplayEl = document.createElement("div");
+                futureDisplayEl.classList = "col-2 future-box"
+
+                // create p items to display info
+                var futureTempDisplay = document.createElement("p");
+                futureTempDisplay.textContent = "Temp: " + futureTemp + " °F";
+                var futureWindDisplay = document.createElement("p");
+                futureWindDisplay.textContent = "Wind: " + futureWind + " MPH";
+                var futureHumidityDisplay = document.createElement("p");
+                futureHumidityDisplay.textContent = "Humidity: " + futureHumidity + "%";
+
+                // append to container
+                futureDisplayEl.appendChild(futureTempDisplay);
+                futureDisplayEl.appendChild(futureWindDisplay);
+                futureDisplayEl.appendChild(futureHumidityDisplay);
+                futureContainerEl.appendChild(futureDisplayEl);
+
+                //futureWeatherConditions(futureTemp, futureWind, futureHumidity)
             };
         })
     })
@@ -60,17 +84,13 @@ var forecastHeader = function(city, date) {
 
 var forecastDisplay = function(temp, humid, windSpeed, UV) {
     var temperature = document.querySelector(".temp");
-    temperature.textContent = temp + " F";
+    temperature.textContent = temp + " °F";
     var humidity = document.querySelector(".humidity");
     humidity.textContent = humid + "%";
     var wind = document.querySelector(".wind");
     wind.textContent = windSpeed + " MPH";
     var uvIndex = document.querySelector(".uv-index");
     uvIndex.textContent = UV;
-}
-// need to figure out how to get each value seperately
-var futureWeatherConditions = function(futureTemp, futureWind, futureHumidity) {
-    console.log(futureTemp)
 }
 
 userFormEl.addEventListener("submit", formSearchHandler);
